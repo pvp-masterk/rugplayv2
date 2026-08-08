1e0add944a5c0de2c2f160859c20195fff018e60
pvp-masterk <imtherealpsx@gmail.com>
2026-08-08 23:26:20 +0500

    Add files via upload

--- website/src/routes/api/arcade/dice/+server.ts
+++ website/src/routes/api/arcade/dice/+server.ts
@@ -100,7 +100,7 @@ export const POST: RequestHandler = async ({ request }) => {
 
         await publishArcadeActivity(userId, result.won ? result.payout : result.amountWagered, result.won, 'dice', 2000);
 
-        await checkAndAwardAchievements(userId, ['arcade', 'wealth'], { arcadeWon: result.won, arcadeWager: result.amountWagered });
+        await checkAndAwardAchievements(userId, ['arcade', 'wealth', 'mythical'], { arcadeWon: result.won, arcadeWager: result.amountWagered });
 
         return json(result);
     } catch (e) {
